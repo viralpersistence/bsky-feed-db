8d2d5a7fee284c9d6291583c99ade6f0e424df13
myociss <yocissms@gmail.com>
2025-02-27 13:50:06 -0600

    added links feed

--- server/algos/__init__.py
+++ server/algos/__init__.py
@@ -1,7 +1,8 @@
-from . import following, discover
+from . import following, discover, links
 
 algos = {
     #feed.uri: feed.handler
     following.uri: following.handler,
     discover.uri: discover.handler,
+    links.uri: links.handler,
 }
