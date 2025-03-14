71c43de33e014baa7ea835c4d08cc64d9a5c1116
viralpersistence <viralpersistence@gmail.com>
2025-03-14 17:10:52 +0000

    fixed issue of older posts not loading

--- server/algos/following.py
+++ server/algos/following.py
@@ -29,7 +29,7 @@ def get_follows(requester_did: str) -> list:
 
 def handler(cursor: Optional[str], limit: int, requester_did: str) -> dict:
 
-    
+
     user = get_or_add_user(requester_did)
 
     '''
@@ -89,12 +89,12 @@ def handler(cursor: Optional[str], limit: int, requester_did: str) -> dict:
 
         where_stmt = (where_stmt & ( ( (Post.indexed_at == indexed_at) & (Post.cid < cid)  ) | (Post.indexed_at < indexed_at) ) )
 
-        posts = Post.select().join(UserFollows, on=(Post.did == UserFollows.follows_did)).where(where_stmt).order_by(Post.cid.desc()).order_by(Post.indexed_at.desc())
-        
-        
+        posts = Post.select().join(UserFollows, on=(Post.did == UserFollows.follows_did)).where(where_stmt).order_by(Post.cid.desc()).order_by(Post.indexed_at.desc()).limit(limit)
+
+
         #posts = [post for post in posts if (post.indexed_at == indexed_at and post.cid < cid) or post.indexed_at < indexed_at]
     else:
-        posts = Post.select().join(UserFollows, on=(Post.did == UserFollows.follows_did)).where(where_stmt).order_by(Post.cid.desc()).order_by(Post.indexed_at.desc())
+        posts = Post.select().join(UserFollows, on=(Post.did == UserFollows.follows_did)).where(where_stmt).order_by(Post.cid.desc()).order_by(Post.indexed_at.desc()).limit(limit)
 
 
     feed = [{'post': post.uri} for post in posts]
